2b67e2bc13c4a91c3bf2fea5bc213d252d0150df
Suman Bhowmick <sumanbhowmick1459@gmail.com>
2023-08-27 23:06:48 +0530

    Create solutions.cpp
    Added solution for day1

---
+++ day1/solutions.cpp
@@ -0,0 +1,18 @@
+//POTD-GFG 27-08-2023
+// Reverse a String
+
+
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        int n=str.length();
+        for(int i=0;i<n/2;i++)
+        {
+            swap(str[i],str[n-i-1]);
+        }
+         return str;
+    }
+   
+};
